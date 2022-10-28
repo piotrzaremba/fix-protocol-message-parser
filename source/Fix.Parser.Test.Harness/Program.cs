8dee4f01687430d00c8d0f23b705498d3cb49dd8
Piotr Zaremba <piotr.zaremba@intlfcstone.com>
2022-10-28 17:01:36 +0000

    10/28/2022 17:01:36

---
+++ source/Fix.Parser.Test.Harness/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Security.Principal;
+using QuickFix;
+using QuickFix.DataDictionary;
+using QuickFix.Fields;
+using QuickFix.FIX44;
+
+namespace Fix.Parser.Test.Harness
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var tcr = new TradeCaptureReport();
+
+                var noClearingInstructions1 = new TradeCaptureReport.NoSidesGroup.NoClearingInstructionsGroup();
+                noClearingInstructions1.ClearingInstruction = new ClearingInstruction(1);
+                var noClearingInstructions2 = new TradeCaptureReport.NoSidesGroup.NoClearingInstructionsGroup();
+                noClearingInstructions2.ClearingInstruction = new ClearingInstruction(2);
+
+                var sidesGrp1 = new TradeCaptureReport.NoSidesGroup();
+                sidesGrp1.Account = new Account("Silvio");
+                sidesGrp1.OrderID = new OrderID("09011900");
+                sidesGrp1.Side = new Side(Side.BUY);
+                sidesGrp1.AddGroup(noClearingInstructions1);
+                sidesGrp1.AddGroup(noClearingInstructions2);
+
+                var sidesGrp2 = new TradeCaptureReport.NoSidesGroup();
+                sidesGrp2.Account = new Account("Sven");
+                sidesGrp2.OrderID = new OrderID("2000");
+                sidesGrp2.Side = new Side(Side.BUY);
+                sidesGrp2.AddGroup(noClearingInstructions2);
+
+                tcr.AddGroup(sidesGrp1);
+                tcr.AddGroup(sidesGrp2);
+
+                var messageStr = tcr.ToString();
+
+                var fixParser = new Parser("./FIX44.xml");
+                var json = fixParser.Parse(messageStr);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+    }
+}
