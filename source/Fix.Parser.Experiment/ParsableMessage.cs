3be8902291f32065bbf7f38dff8d637eeaea5bb2
Piotr Zaremba <piotr.zaremba@intlfcstone.com>
2022-10-28 19:04:34 +0000

    10/28/2022 19:04:34

---
+++ source/Fix.Parser.Experiment/ParsableMessage.cs
@@ -0,0 +1,147 @@
+﻿using System.Diagnostics;
+using System.Text;
+using QuickFix;
+using QuickFix.DataDictionary;
+using QuickFix.Fields;
+
+namespace Fix.Parser.Experiment
+{
+    public class ParsableMessage : Message
+    {
+        public ParsableMessage() {}
+
+        public ParsableMessage(string msgstr) : this(msgstr, true){}
+        
+        public ParsableMessage(string msgstr, bool validate) : base(msgstr, null, null, validate) { }
+        
+        public ParsableMessage(string msgstr, DataDictionary dataDictionary, bool validate) : base(msgstr, dataDictionary, validate) { }
+
+
+        public ParsableMessage(string msgstr, DataDictionary sessionDataDictionary, DataDictionary appDD, bool validate) : base(msgstr, sessionDataDictionary, appDD, validate) { }
+
+        public ParsableMessage(Message src) : base(src) { }
+
+
+        private static StringBuilder FieldMapToJSON(StringBuilder sb, DataDictionary dd, FieldMap fields, bool humanReadableValues)
+        {
+            IList<int> numInGroupTagList = fields.GetGroupTags();
+            IList<IField> numInGroupFieldList = new List<IField>();
+            string valueDescription = string.Empty;
+
+            // Non-Group Fields
+            foreach (var field in fields)
+            {
+                if (dd.FieldsByTag[field.Value.Tag].Name == "NoSides")
+                {
+                    Debug.WriteLine("");
+                }
+
+                if (QuickFix.Fields.CheckSum.TAG == field.Value.Tag)
+                    continue; // FIX JSON Encoding does not include CheckSum
+
+                if (numInGroupTagList.Contains(field.Value.Tag))
+                {
+                    numInGroupFieldList.Add(field.Value);
+                    continue; // Groups will be handled below
+                }
+
+                if ((dd != null) && (dd.FieldsByTag.ContainsKey(field.Value.Tag)))
+                {
+                    sb.Append("\"" + dd.FieldsByTag[field.Value.Tag].Name + field.Value.Tag + "\":");
+                    if (humanReadableValues)
+                    {
+                        if (dd.FieldsByTag[field.Value.Tag].EnumDict.TryGetValue(field.Value.ToString(), out valueDescription))
+                        {
+                            sb.Append("\"" + valueDescription + "\",");
+                        }
+                        else
+                        {
+                            sb.Append("\"" + field.Value.ToString() + "\",");
+                        }
+                    }
+                    else
+                    {
+                        sb.Append("\"" + field.Value.ToString() + "\",");
+                    }
+                }
+                else
+                {
+                    sb.Append("\"" + field.Value.Tag.ToString() + "\":");
+                    sb.Append("\"" + field.Value.ToString() + "\",");
+                }
+            }
+
+            // Group Fields
+            foreach (IField numInGroupField in numInGroupFieldList)
+            {
+                // The name of the NumInGroup field is the key of the JSON list containing the Group items
+                if ((dd != null) && (dd.FieldsByTag.ContainsKey(numInGroupField.Tag)))
+                    sb.Append("\"" + dd.FieldsByTag[numInGroupField.Tag].Name + dd.FieldsByTag[numInGroupField.Tag].Tag + "\":[");
+                else
+                    sb.Append("\"" + numInGroupField.Tag.ToString() + "\":[");
+
+                // Populate the JSON list with the Group items
+                for (int counter = 1; counter <= fields.GroupCount(numInGroupField.Tag); counter++)
+                {
+                    sb.Append("{");
+                    FieldMapToJSON(sb, dd, fields.GetGroup(counter, numInGroupField.Tag), humanReadableValues);
+                    sb.Append("},");
+                }
+
+                // Remove trailing comma
+                if (sb.Length > 0 && sb[sb.Length - 1] == ',')
+                    sb.Remove(sb.Length - 1, 1);
+
+                sb.Append("],");
+            }
+            // Remove trailing comma
+            if (sb.Length > 0 && sb[sb.Length - 1] == ',')
+                sb.Remove(sb.Length - 1, 1);
+
+            return sb;
+        }
+
+
+        /// <summary>
+        /// Get a representation of the message as a string in FIX JSON Encoding.
+        /// See: https://github.com/FIXTradingCommunity/fix-json-encoding-spec
+        /// </summary>
+        /// <returns>a JSON string</returns>
+        public string ToJSON()
+        {
+
+            return ToJSON(this.dataDictionary_, false);
+        }
+
+
+        /// <summary>
+        /// Get a representation of the message as a string in FIX JSON Encoding.
+        /// See: https://github.com/FIXTradingCommunity/fix-json-encoding-spec
+        ///
+        /// Per the FIX JSON Encoding spec, tags are converted to human-readable form, but values are not.
+        /// If you want human-readable values, set humanReadableValues to true.
+        /// </summary>
+        /// <returns>a JSON string</returns>
+        public string ToJSON(bool humanReadableValues)
+        {
+            return ToJSON(this.dataDictionary_, humanReadableValues);
+        }
+
+        /// <summary>
+        /// Get a representation of the message as a string in FIX JSON Encoding.
+        /// See: https://github.com/FIXTradingCommunity/fix-json-encoding-spec
+        ///
+        /// Per the FIX JSON Encoding spec, tags are converted to human-readable form, but values are not.
+        /// If you want human-readable values, set humanReadableValues to true.
+        /// </summary>
+        /// <returns>a JSON string</returns>
+        public string ToJSON(DataDictionary dd, bool humanReadableValues)
+        {
+            StringBuilder sb = new StringBuilder().Append("{").Append("\"Header\":{");
+            FieldMapToJSON(sb, dd, Header, humanReadableValues).Append("},\"Body\":{");
+            FieldMapToJSON(sb, dd, this, humanReadableValues).Append("},\"Trailer\":{");
+            FieldMapToJSON(sb, dd, Trailer, humanReadableValues).Append("}}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ source/Fix.Parser.Experiment/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using QuickFix.DataDictionary;
+using QuickFix.Fields;
+using QuickFix.FIX44;
+
+namespace Fix.Parser.Experiment
+{
+    class Program
+    {
+        static void FixToJson(string messageStr, bool humanReadableValues, QuickFix.DataDictionary.DataDictionary sessionDataDictionary, QuickFix.DataDictionary.DataDictionary appDataDictionary)
+        {
+            try
+            {
+
+                    QuickFix.IMessageFactory msgFactory = new QuickFix.DefaultMessageFactory();
+                    ParsableMessage msg = new Fix.Parser.Experiment.ParsableMessage();
+                    string line = messageStr;
+                    string comma = "";
+
+                        line = line.Trim();
+                        msg.FromString(line, false, sessionDataDictionary, appDataDictionary, msgFactory);
+                        Console.WriteLine(comma + msg.ToJSON(sessionDataDictionary, humanReadableValues));
+
+                        comma = ",";
+     
+
+
+
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 4)
+            {
+                System.Console.WriteLine("USAGE");
+                System.Console.WriteLine("");
+                System.Console.WriteLine("    FixToJson.exe FILE [HUMAN_READABLE_VALUES] [DATA_DICTIONARY]");
+                System.Console.WriteLine("");
+                System.Console.WriteLine("EXAMPLES");
+                System.Console.WriteLine("");
+                System.Console.WriteLine("    FixToJson.exe messages.log true ../../spec/fix/FIX50SP2.xml");
+                System.Console.WriteLine("    FixToJson.exe messages.log true ../../spec/fix/FIX44.xml");
+                System.Console.WriteLine("    FixToJson.exe messages.log false ../../spec/fix/FIX42.xml");
+                System.Console.WriteLine("");
+                System.Console.WriteLine("NOTE");
+                System.Console.WriteLine("");
+                System.Console.WriteLine("    Per the FIX JSON Encoding Specification, tags are converted to human-readable form, but values are not.");
+                System.Console.WriteLine("    Set the HUMAN_READABLE_VALUES argument to TRUE to override the standard behavior.");
+                //System.Environment.Exit(2);
+            }
+
+            /*
+            string fname = args[0];
+            bool humanReadableValues = false;
+            QuickFix.DataDictionary.DataDictionary sessionDataDictionary = null;
+            QuickFix.DataDictionary.DataDictionary appDataDictionary = null;
+
+            if (args.Length > 1)
+            {
+                humanReadableValues = bool.Parse(args[1]);
+            }
+
+            if (args.Length > 2)
+            {
+                sessionDataDictionary = new QuickFix.DataDictionary.DataDictionary(args[2]);
+                appDataDictionary = sessionDataDictionary;
+            }
+            */
+
+            var tcr = new TradeCaptureReport();
+
+            var noClearingInstructions1 = new TradeCaptureReport.NoSidesGroup.NoClearingInstructionsGroup();
+            noClearingInstructions1.ClearingInstruction = new ClearingInstruction(1);
+            var noClearingInstructions2 = new TradeCaptureReport.NoSidesGroup.NoClearingInstructionsGroup();
+            noClearingInstructions2.ClearingInstruction = new ClearingInstruction(2);
+
+            var sidesGrp1 = new TradeCaptureReport.NoSidesGroup();
+            sidesGrp1.Account = new Account("Silvio");
+            sidesGrp1.OrderID = new OrderID("09011900");
+            sidesGrp1.Side = new Side(Side.BUY);
+            sidesGrp1.AddGroup(noClearingInstructions1);
+            sidesGrp1.AddGroup(noClearingInstructions2);
+
+            var sidesGrp2 = new TradeCaptureReport.NoSidesGroup();
+            sidesGrp2.Account = new Account("Sven");
+            sidesGrp2.OrderID = new OrderID("2000");
+            sidesGrp2.Side = new Side(Side.BUY);
+            sidesGrp2.AddGroup(noClearingInstructions2);
+
+            tcr.AddGroup(sidesGrp1);
+            tcr.AddGroup(sidesGrp2);
+
+            var messageStr = tcr.ToString();
+
+            var _dataDictionary = new DataDictionary("FIX44.xml");
+
+            FixToJson(messageStr, false, _dataDictionary, _dataDictionary);
+            Environment.Exit(1);
+        }
+    }
+}
